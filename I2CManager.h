d45bdba0766ddb4c5cabec66b67581f7547a7a91
ntankard <nicholas_tankard@hotmail.com>
2015-08-23 22:48:50 +1000

    I2C buss added

---
+++ I2CManager.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "SharedBusManager.h"
+
+class I2CManager:public SharedBusManager
+{
+public:
+	I2CManager(Error *theError);
+	void read(uint8_t address, uint8_t start, uint8_t *buffer, int size);
+	void write(uint8_t address, uint8_t start, uint8_t *buffer, int size);
+};
+
