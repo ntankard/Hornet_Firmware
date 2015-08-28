d8776114b8d1243395bf0f4562a3c30fc2241d3c
ntankard <nicholas_tankard@hotmail.com>
2015-08-28 11:40:12 +1000

    APM_Indicator complete

--- APM_Indicator.cpp
+++ APM_Indicator.cpp
@@ -13,12 +13,16 @@ APM_Indicator::APM_Indicator() :Indicator()
 	digitalWrite(YELLOY_LED, OFF);
 }
 
+//-----------------------------------------------------------------------------------------------------------------------------
+
 void APM_Indicator::lightOn(int setting)
 {
 	digitalWrite(BLUE_LED, onOffState[setting][0]);
 	digitalWrite(RED_LED, onOffState[setting][1]);
 	digitalWrite(YELLOY_LED, onOffState[setting][2]);
 }
+
+//-----------------------------------------------------------------------------------------------------------------------------
 
 void APM_Indicator::lightOff()
 {
@@ -28,170 +32,3 @@ void APM_Indicator::lightOff()
 }
 
 #endif
-
-/*
-
-#include "Indicator.h"
-#include "Arduino.h"
-#include "APM_2_5_PINS.h"
-#include "CONFIG.h"
-
-#if ENABLE_INDICATOR == ENABLED
-
-Indicator::Indicator()
-{
-	_isOn = false;
-	_c = BLUE;
-	_blinks = 1;
-	_rate = 1000;
-	_isBreak = false;
-
-	pinMode(BLUE_LED, OUTPUT);
-	digitalWrite(BLUE_LED, OFF);
-	pinMode(RED_LED, OUTPUT);
-	digitalWrite(RED_LED, OFF);
-	pinMode(YELLOY_LED, OUTPUT);
-	digitalWrite(YELLOY_LED, OFF);
-
-	_pin[0] = BLUE_LED;
-	_pin[1] = YELLOY_LED;
-	_pin[2] = RED_LED;
-
-	for (int i = 0; i < 3; i++)
-	{
-	_interval[i]=500;
-	_isOn[i] = false;
-	_blinks[i] = 3;
-	_pinState[i] = OFF;
-	pinMode(_pin[i], OUTPUT);
-	digitalWrite(_pin[i], _pinState[i]);
-	}
-
-}
-
-void Indicator::run()
-{
-	unsigned long currentMillis = millis();
-
-	if (_isOn)
-	{
-		if (_isBreak)
-		{
-			if (currentMillis >= (_pastTime + (_rate * 3)))
-			{
-				// end of a break
-				_pinState = ON;
-				lightOn();
-				_pastTime = currentMillis;
-				_isBreak = false;
-				_doneBlinks = 0;
-			}
-		}
-		else
-		{
-			if (currentMillis >= (_pastTime + _rate))
-			{
-				if (_pinState == OFF)
-				{
-					// end of a regular gap
-					_pinState = ON;
-					lightOn();
-					_pastTime = currentMillis;
-				}
-				else
-				{
-					// end of a on period
-					_pinState = OFF;
-					lightOff();
-					_pastTime = currentMillis;
-					if (_blinks != 1)
-					{
-						// pattern
-						_doneBlinks++;
-						if (_doneBlinks >= _blinks)
-						{
-							// time for a long break
-							_isBreak = true;
-						}
-					}
-				}
-			}
-		}
-	}
-}
-
-
-
-
-void Indicator::on()
-{
-	_isOn = true;
-
-	if (_isOn)
-	{
-		// reset
-		_isBreak = false;
-		_doneBlinks = 0;
-		_pastTime = millis();
-	}
-}
-
-void Indicator::off()
-{
-	lightOff();
-	_isOn = false;
-}
-
-void Indicator::setDisplay(Color c, int blinks, unsigned long rate)
-{
-	_c = c;
-	_blinks = blinks;
-	_rate = rate;
-}
-
-void Indicator::lightOn()
-{
-	digitalWrite(BLUE_LED, onOffState[_c][0]);
-	digitalWrite(RED_LED, onOffState[_c][1]);
-	digitalWrite(YELLOY_LED, onOffState[_c][2]);
-}
-
-void Indicator::lightOff()
-{
-	digitalWrite(BLUE_LED, OFF);
-	digitalWrite(RED_LED, OFF);
-	digitalWrite(YELLOY_LED, OFF);
-}
-
-/*
-void Indicator::setOn(Color c, bool isOn)
-{
-
-if (_isOn[c] != isOn)
-{
-if (isOn)
-{
-// reset
-_isBreak[c] = false;
-_doneBlinks[c] = 0;
-_pastTime[c] = millis();
-}
-_isOn[c] = isOn;
-_pinState[c] = OFF;
-digitalWrite(_pin[c], _pinState[c]);
-}
-}
-
-void Indicator::setBlinks(Color c, int b)
-{
-_blinks[c] = b;
-}
-
-void Indicator::setBlinkRate(Color c, int r)
-{
-_interval[c] = r;
-}
-
-
-#endif
-*/
--- APM_Indicator.h
+++ APM_Indicator.h
@@ -1,110 +1,23 @@
 #pragma once
-#include "Indicator.h"
 #include "CONFIG.h"
 
-#include "arduino.h"
-
 #ifdef USE_APM_INDICATOR
+
+#include "Indicator.h"
+#include "arduino.h"
 
 class APM_Indicator :public Indicator
 {
 public:
+	enum Color{ BLUE =0, YELLOW=1, RED=2, PURPLE=3, ORANGE=4, MAGENTA=5};
 	APM_Indicator();
 protected:
 	void lightOn(int setting);
 	void lightOff();
+
 	const int ON = LOW;
 	const int OFF = HIGH;
 	const int onOffState[6][3] = { { ON, OFF, OFF }, { OFF, OFF, ON }, { ON, OFF, ON }, { ON, OFF, ON }, { OFF, ON, ON }, { ON, ON, OFF } };
 };
 
 #endif
-
-/*
-#pragma once
-
-#include "arduino.h"
-
-#if ENABLE_INDICATOR == ENABLED
-
-class Indicator {
-
-public:
-
-	enum Color{ BLUE, YELLOW, RED, PURPLE, ORANGE, MAGENTA };
-
-	//blue red yellow
-	const int onOffState[6][3] = { { ON, OFF, OFF }, { OFF, OFF, ON }, { ON, OFF, ON }, { ON, OFF, ON }, { OFF, ON, ON }, { ON, ON, OFF } };
-
-	Indicator();
-
-	void run();
-
-	void on();
-
-	void off();
-
-	void setDisplay(Color c, int blinks, unsigned long rate);
-
-
-	void setOn(Color c, bool isOn);
-
-	void setBlinks(Color c, int b);
-
-	void setBlinkRate(Color c, int r);
-
-
-
-private:
-
-
-	void lightOn();
-
-	void lightOff();
-
-
-	bool _isOn;
-	Color _c;
-	int _blinks;
-	unsigned long _rate;
-	unsigned long _pastTime;
-	int _pinState;
-	bool _isBreak;
-	int _doneBlinks;
-
-	//unsigned long _interval;
-
-	//const int BLUE = 0;
-	//const int YELLOW = 1;
-	//const int RED = 2;
-
-	unsigned long _interval[3];
-	bool _isOn[3];
-	int _blinks[3];
-	int _pin[3];
-	int _pinState[3];
-
-	bool _isBreak[3];
-	int _doneBlinks[3];
-	unsigned long _pastTime[3];
-
-	const int ON = LOW;
-	const int OFF = HIGH;
-};
-
-#else
-class Indicator {
-
-public:
-	enum Color{ BLUE, YELLOW, RED, PURPLE, ORANGE, MAGENTA };
-	Indicator(){}
-	void run(){}
-	void on(){}
-	void off(){}
-	void setDisplay(Color c, int blinks, unsigned long rate){}
-	void setOn(Color c, bool isOn){}
-	void setBlinks(Color c, int b){}
-	void setBlinkRate(Color c, int r){}
-};
-#endif
-*/
