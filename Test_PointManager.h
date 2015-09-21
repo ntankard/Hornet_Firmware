2e626593c43ac4408c1c2068a5ba9bfbb14457cf
matthannah <mryanhannah@gmail.com>
2015-09-21 18:48:06 +1000

    --

--- Test_PointManager.h
+++ Test_PointManager.h
@@ -7,6 +7,15 @@
 test(PointManager_Setup)
 {
 	PointManager* _pointManager = new PointManager();
+	DoublyLinkedNodeIterator<Point> iter = DoublyLinkedNodeIterator<Point>(*_pointManager->gePointList());
+	assertEqual(iter.getNode()->getValue().getState(), HEAD);
+	for (int i = 0; i < L_POINTS_IN_PATTERN; i++)
+	{
+		iter++;
+		assertEqual(iter.getNode()->getValue().getState(), NULLPOINT);
+	}
+	iter++;
+	assertEqual(iter.getNode()->getValue().getState(), TAIL);
 }
 
 #endif
